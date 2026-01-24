8831f72fad8f04d6c63679114f4dca150f0d16eb
SeguisP <seguis.cs.jun@gmail.com>
2026-01-24 22:05:57 +0100

    inicializar nuevo proyecto y añadir struct acreditado

---
+++ backend/persona.go
@@ -0,0 +1,18 @@
+package main
+
+type Acreditado struct {
+	//Se supone que fronted no envia id, pero backend si debe responder con uno
+	ID string `json:"id,omitempty" firestore:"-"`
+
+	//Datos de la persona (Foto opcional)
+	Nombre    string `json:"nombre" firestore:"nombre" validate:"required"`
+	Apellidos string `json:"apellidos" firestore:"apellidos" validate:"required"`
+	Email     string `json:"email" firestore:"email" validate:"required"`
+	DNI       string `json:"dni" firestore:"dni" validate:"required"`
+	Empresa   string `json:"empresa" firestore:"empresa" validate:"required"`
+	Foto      string `json:"foto,omitempty" firestore:"foto,omitempty"`
+
+	//Variables de creacion y modificacion
+	Created  int64 `json:"created" firestore:"created"`
+	Modified int64 `json:"modified" firestore:"modified"`
+}
