f756961b374e80d729c4258f48378fee9fbe87e5
SeguisP <seguis.cs.jun@gmail.com>
2026-01-31 19:15:00 +0100

    feat: Conectar firestore

--- backend/go.mod
+++ backend/go.mod
@@ -3,11 +3,14 @@ module practicas/exyt/backend
 go 1.25.6
 
 require (
+	github.com/go-playground/validator/v10 v10.30.1
+	github.com/labstack/echo/v4 v4.15.0
+)
+
+require (
 	github.com/gabriel-vasile/mimetype v1.4.12 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.30.1 // indirect
-	github.com/labstack/echo/v4 v4.15.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os"
 	"time"
 
+	"cloud.google.com/go/firestore"
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-// Integrar validador con Echo
+// CustomValidator integra el validador con Echo
 type CustomValidator struct {
 	validator *validator.Validate
 }
@@ -21,37 +25,60 @@ func (cv *CustomValidator) Validate(i interface{}) error {
 func main() {
 	e := echo.New()
 
-	// Middleware (logs y recuperación de errores)
-	e.Use(middleware.Logger())
+	// Middleware
+	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogStatus: true,
+		LogURI:    true,
+		LogMethod: true,
+		LogError:  true,
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			log.Printf("REQUEST: method=%v uri=%v status=%v error=%v\n", v.Method, v.URI, v.Status, v.Error)
+			return nil
+		},
+	}))
 	e.Use(middleware.Recover())
 
-	// Configuración del validator
 	e.Validator = &CustomValidator{validator: validator.New()}
 
-	// Ruta de prueba para el test
+	// *** INICIO CONFIGURACION FIRESTORE ***
+	ctx := context.Background()
+
+	//Actualizada funcion para buscar las llaves de la BD
+	os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", "llavesBd.json")
+
+	// Inicializar bd
+	client, err := firestore.NewClientWithDatabase(ctx, "pf26-seguis-rafael-lopez", "practicas")
+	if err != nil {
+		log.Fatalf("Error inicializando firestore: %v\n", err)
+	}
+	defer client.Close()
+	// *** FIN CONFIGURACIÓN FIRESTORE ***
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hola Exyt")
 	})
 
-	// Se crea acreditado
 	e.POST("/acreditados", func(c echo.Context) error {
-		// Usamos el struct definido en persona.go
 		acreditado := new(Acreditado)
 
-		// Juntar los datos
 		if err := c.Bind(acreditado); err != nil {
 			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 		}
 
-		// Validar campos requeridos
 		if err := c.Validate(acreditado); err != nil {
 			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 		}
 
-		// Hora del sistema
 		now := time.Now().Unix()
 		acreditado.Created = now
 		acreditado.Modified = now
+
+		ref, _, err := client.Collection("acreditado").Add(ctx, acreditado)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, "Error guardando en BBDD: "+err.Error())
+		}
+
+		acreditado.ID = ref.ID
 
 		return c.JSON(http.StatusCreated, acreditado)
 	})
